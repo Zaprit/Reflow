6dc8dbe8a6ce3d15c9ed6ed21837f1c86b42214c
Henry Asbridge <henry@vorax.org>
2021-09-19 23:14:37 +0100

    Added API key function

--- technicapi/key.go
+++ technicapi/key.go
@@ -1,7 +1,50 @@
 package technicapi
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/Zaprit/Reflow/database"
+	"github.com/Zaprit/Reflow/models"
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
+	"net/http"
+)
 
-func VerifyKey(w http.ResponseWriter, req http.Request) {
+// VerifyKey is the endpoint that verifies a key
+func VerifyKey(w http.ResponseWriter, req *http.Request) {
 
+	vars := mux.Vars(req)
+
+	var key models.APIKey
+
+	result := database.GetDBInstance().Take(&key, "api_key = ?", vars["key"])
+
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		fmt.Errorf("this is not nessecerally an error, it just means that someone used the wrong API key, " +
+			"check it's been entered correctly and try again\n")
+		_, err := w.Write(models.APIErrorJSON("Invalid key provided."))
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		return
+	}
+
+	out, err := json.Marshal(models.APIKeyVerifyResponse{
+		Valid:     "Key validated.",
+		Name:      key.Name,
+		CreatedAt: key.CreatedAt,
+	})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	_, err = w.Write(out)
+
+	if err != nil {
+		panic(err.Error())
+	}
 }
