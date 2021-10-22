8cddd49bb9633e653e7e143f49909303ae04069c
Henry Asbridge <henry@vorax.org>
2021-10-22 21:44:58 +0100

    Added json marshal wrapper for debug pretty-print

--- utils/jsonMarshal.go
+++ utils/jsonMarshal.go
@@ -1,1 +1,29 @@
 package utils
+
+import (
+	"encoding/json"
+	"github.com/Zaprit/Reflow/config"
+)
+
+func Marshal(v interface{}) []byte {
+
+	serverConf, err := config.Conf.Section("server")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	debug := serverConf.ValueOf("debug")
+
+	var out []byte
+
+	if debug == "true" || debug == "yes" {
+		out, err = json.MarshalIndent(v, "", "\t")
+	} else {
+		out, err = json.Marshal(v)
+	}
+	if err != nil {
+		panic(err.Error())
+	}
+	return out
+}
