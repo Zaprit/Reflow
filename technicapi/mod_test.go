d7fb3a9f9006c9a9eb2d63bef1b62da10aececc2
Zaprit <henry@vorax.org>
2021-10-25 23:17:44 +0100

    Added getMods test

--- technicapi/mod_test.go
+++ technicapi/mod_test.go
@@ -72,5 +72,68 @@ func TestGetModVersion(t *testing.T) {
 }
 
 func TestGetMods(t *testing.T) {
+	database.CreateDBInstance(&database.DBConfig{
+		Driver: "sqlite",
+		DBName: "file::memory:?cache=shared",
+	})
 
+	dbMod := models.Mod{
+		DBStructTemplate: models.DBStructTemplate{CreatedAt: time.Now()},
+		Name:             "test-mod",
+		Description:      "Test Mod Description",
+		Author:           "Test Author",
+		Link:             "https://example.com",
+		DisplayName:      "Test Mod",
+	}
+	dbMod2 := models.Mod{
+		DBStructTemplate: models.DBStructTemplate{CreatedAt: time.Now()},
+		Name:             "test-mod-two",
+		Description:      "Second Test Mod Description",
+		Author:           "Second Test Author",
+		Link:             "https://example.org",
+		DisplayName:      "Second Test Mod",
+	}
+	dbMod3 := models.Mod{
+		DBStructTemplate: models.DBStructTemplate{CreatedAt: time.Now()},
+		Name:             "test-mod-three",
+		Description:      "Third Test Mod Description",
+		Author:           "Third Test Author",
+		Link:             "https://example.net",
+		DisplayName:      "Third Test Mod",
+	}
+
+	database.GetDBInstance().Create(&dbMod)
+	database.GetDBInstance().Create(&dbMod2)
+	database.GetDBInstance().Create(&dbMod3)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/api/mod", GetMod)
+
+	ts := httptest.NewServer(r)
+
+	body, err := internal.TestClient(ts.URL + "/api/mod")
+
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	var modlist models.ModList
+
+	er2 := json.Unmarshal(body, &modlist)
+	if er2 != nil {
+		t.Fatal(er2.Error())
+	}
+
+	mods := map[string]string{
+		dbMod.Name:  dbMod.DisplayName,
+		dbMod2.Name: dbMod2.DisplayName,
+		dbMod3.Name: dbMod3.DisplayName,
+	}
+
+	for name, prettyName := range modlist.Mods {
+		if mods[name] != prettyName {
+			t.Errorf("While checking modlist response unexpected mod, expected {%s:%s}, got {%s:%s}",
+				name, mods[name], name, prettyName)
+		}
+	}
 }
