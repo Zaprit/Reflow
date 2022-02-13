932178959cf321d18fa4dd92ee21b28f86577985
Zaprit <henry@vorax.org>
2022-02-13 00:53:45 +0000

    Added janky test handling

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -2,16 +2,28 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"reflect"
 	"testing"
+
+	"github.com/Zaprit/Reflow/config"
 )
 
-func TestMarshal(t *testing.T) {
+type teststruct struct {
+	Id   int
+	Name string
+}
 
-	type teststruct struct {
-		Id   int
-		Name string
-	}
+type testStructWithBadJson struct {
+	Id   int
+	Name string
+}
+
+func (s testStructWithBadJson) MarshalJSON() ([]byte, error) {
+	return nil, fmt.Errorf("sand")
+}
+
+func TestMarshal(t *testing.T) {
 
 	data := teststruct{
 		Id:   1,
@@ -28,5 +40,31 @@ func TestMarshal(t *testing.T) {
 	}
 	if !reflect.DeepEqual(data, outdata) {
 		t.Errorf("Structs NOT equal, utils.Marshal does not work")
+	}
+
+	config.Conf.Server.Debug = true
+
+	jsondata2 := Marshal(&data)
+
+	outdata = teststruct{}
+
+	err = json.Unmarshal(jsondata2, &outdata)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if !reflect.DeepEqual(data, outdata) {
+		t.Errorf("Structs NOT equal, utils.Marshal does not work")
+	}
+
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("panic occurred:", err, "- this was expected")
+		}
+	}()
+
+	jsondata3 := Marshal(testStructWithBadJson{})
+	if jsondata3 != nil {
+		//		t.Error("was expecting a failure, wtf")
 	}
 }
